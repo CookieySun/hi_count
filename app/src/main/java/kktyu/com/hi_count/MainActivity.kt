7b7cd4998fe2457c7537e8856f595bf33722b1fe
kukita <ku.k669500@gmail.com>
2019-03-12 20:40:12 +0900

    🎉 Initial Commit

---
+++ app/src/main/java/kktyu/com/hi_count/MainActivity.kt
@@ -0,0 +1,12 @@
+package kktyu.com.hi_count
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
